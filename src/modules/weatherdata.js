6b27f918bcfc28a66e665d5fe96643c0292859ce
Abdirazakf <farahabdirazak13@gmail.com>
2025-08-08 20:06:22 -0400

    Added search results based on user input as well as updating the header to the search location (with New York as the default)

--- src/modules/weatherdata.js
+++ src/modules/weatherdata.js
@@ -6,11 +6,35 @@ export default class WeatherData {
         this.currentTemp = null
         this.currentCondition = null
         this.isLoaded = false
+        this.search = 'New York'
+    }
+
+    getSearchResult() {
+        const form = document.querySelector('form')
+        const searchResult = form.querySelector('input')
+        const locationHeader = document.querySelector('.location')
+
+        form.addEventListener('submit', async (event) => {
+            event.preventDefault()
+
+            if (searchResult.value){
+                this.search = searchResult.value
+            }
+
+            await this.getData()
+            locationHeader.textContent = this.weatherData.resolvedAddress.charAt(0).toUpperCase() + this.weatherData.resolvedAddress.slice(1)
+            form.reset()
+        })
+
+        return this.search
     }
 
     async getData(){
+        this.getSearchResult()
+
         try {
-            const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/atlanta?key=292MCQGT7NMZ6WDTLJE6UDWLQ&include=days,current", {
+            console.log(this.search)
+            const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${this.search}?key=292MCQGT7NMZ6WDTLJE6UDWLQ&include=days,current`, {
                 mode: 'cors'
             })
             
@@ -22,6 +46,8 @@ export default class WeatherData {
             this.currentCondition = data.currentConditions.conditions
             this.currentTemp = data.currentConditions.temp
             this.isLoaded = true
+
+            this.checkData()
             
             return {
                 weatherData: this.weatherData,
@@ -38,9 +64,8 @@ export default class WeatherData {
     checkData() {
         if (!this.isLoaded) {
             throw new Error('Weather data not loaded yet');
-        }
-
-        if (this.weatherData) {
+        } else {
+            console.log(this.weatherData)
             console.log(this.daysData)
             console.log(this.currentDate)
             console.log(this.currentTemp)
@@ -48,8 +73,14 @@ export default class WeatherData {
         }
     }
 
+    displayCurrentData(){
+        const header = document.querySelector('.location')
+        header.textContent = this.search
+    }
+
     async init() {
+        this.getSearchResult()
         await this.getData()
-        this.checkData()
+        this.displayCurrentData()
     }
 }
